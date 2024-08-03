d5d34d008a0924f76930946f2cafc51d178384b9
austinletson <waustinletson@gmail.com>
2024-08-03 08:06:11 -0400

    lake new

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LakeInternalApiExample
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package "lake-internal-api-example" where
+  -- add package configuration options here
+
+lean_lib «LakeInternalApiExample» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe "lake-internal-api-example" where
+  root := `Main
